149d6c62b85d6d46f8982263fe2244bad2ed8ce0
aligolbabaee96 <ali.golbabaeek22@gmail.com>
2017-08-12 21:43:06 +0430

    felan hichi nashode

---
+++ apis.md
@@ -0,0 +1,8 @@
+/submit/expense/
+     post. return a json
+     input: date(optional) ,text,amount,user
+     output: status : ok
+/submit/income/
+  post. return a json
+  input: date(optional) ,text,amount,user
+  output : status : ok
--- golco/models.py
+++ golco/models.py
@@ -5,11 +5,20 @@ from django.db import models
 
 
 # Create your models here.
+#############################################################
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length = 120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50) #TODO: do not save password
+###############################################################
 class Token(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     token=models.CharField(max_length=48)
     def __unicode__(self):
         return self.user
+##############################################################
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -17,6 +26,7 @@ class Expense(models.Model):
     user = models.ForeignKey(User)
     def __unicode__(self):
         return "() - () ".format(self.date, self.text)
+################################################################
 class Income(models.Model):
     text= models.CharField(max_length=255)
     date = models.DateTimeField()
